45eca6240751ce537e1f5bc8d4ac817acde2fd49
IgorPropp <igor.propp001@yandex.ru>
2024-05-13 20:24:06 +0500

    feat: add logging for stats app

--- stats/server/src/main/java/ru/practicum/controller/StatsController.java
+++ stats/server/src/main/java/ru/practicum/controller/StatsController.java
@@ -1,6 +1,7 @@
 package ru.practicum.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.model.EndpointHit;
@@ -15,12 +16,14 @@ import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class StatsController {
 
     private final StatsService statsService;
 
     @PostMapping("/hit")
     public void createEndPointHit(@RequestBody EndpointHit endpointHit) {
+        log.info("POST endpointHit app={}, ip={}, uri={}", endpointHit.getApp(), endpointHit.getIp(), endpointHit.getUri());
         statsService.createEndpointHit(endpointHit);
     }
 
@@ -29,17 +32,15 @@ public class StatsController {
                                         @RequestParam("end") Optional<String> end,
                                         @RequestParam("uris") Optional<List<String>> uris,
                                         @RequestParam(name = "unique", defaultValue = "false") Boolean unique) {
+        log.info("GET statsView start={}, end={}, uris={}", start, end, uris);
         return statsService.getStatsView(start, end, uris, unique);
     }
 
     @GetMapping("/statsByIp")
-    public ResponseEntity<List<StatsView>> statsByIp(@RequestParam String start,
-                                                     @RequestParam String end,
+    public ResponseEntity<List<StatsView>> statsByIp(@RequestParam String start, @RequestParam String end,
                                                      @RequestParam(required = false) List<String> uris,
                                                      @RequestParam(defaultValue = "false") Boolean unique,
                                                      @RequestParam String ip) {
-
-
         LocalDateTime startDT;
         LocalDateTime endDT;
         try {
@@ -50,6 +51,7 @@ public class StatsController {
         }
 
         List<StatsView> results = statsService.getRequestsWithViewsByIp(startDT, endDT, uris, unique, ip);
+        log.info("GET statsByIp start={}, end={}, ip={}, uris={}, unique={}", start, end, ip, uris, unique);
         return ResponseEntity.ok().body(results);
     }
 }
